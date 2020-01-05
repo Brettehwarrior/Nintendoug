7cc89a2f6be16aeb4ea335cd68a1686d4f7385b2
Trent <Brettehwarrior@outlook.com>
2020-01-05 19:44:13 -0400

    ;)

---
+++ Assets/Scripts/Doug.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Doug : MonoBehaviour
+{
+    public Transform pointOfInterest;
+    public CharacterController controller;
+
+    public float moveSpeed;
+
+    void Update() {
+        Move();
+    }
+
+    void Move() {
+        var offset = pointOfInterest.position - transform.position;
+        offset.y = 0f;
+
+        if (offset.magnitude > .1f) {
+            transform.rotation = Quaternion.LookRotation(offset);
+            offset = offset.normalized * moveSpeed;
+            controller.Move(offset * Time.deltaTime);
+        }
+    }
+}
