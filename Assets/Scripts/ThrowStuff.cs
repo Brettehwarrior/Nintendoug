4d5b23e10f8f12fcb52855f920a9d0f635ddfd20
Trent <Brettehwarrior@outlook.com>
2020-01-05 20:11:19 -0400

    he chase sball now

---
+++ Assets/Scripts/ThrowStuff.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowStuff : MonoBehaviour
+{
+    public GameObject doug;
+    public GameObject ball;
+    public float launchSpeed;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit)) {
+                LaunchBall(hit.point);
+            }
+        }
+    }
+
+    void LaunchBall(Vector3 targetDirection) {
+        var b = Instantiate(ball, Camera.main.transform.position, Quaternion.identity);
+        b.GetComponent<Rigidbody>().AddForce(targetDirection * launchSpeed);
+
+        doug.GetComponent<Doug>().pointOfInterest = b.transform;
+    }
+}
